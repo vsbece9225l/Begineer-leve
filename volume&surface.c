7fe1627cd8931e631cbf34312e90c2559a9ad054
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-09 16:28:58 +0530

    Create volume&surface.c

---
+++ volume&surface.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int len,width,height;
+ printf("enter the length,height,width of cuboid");
+ scanf("%d%d%d",&len,&width,&height);
+ volume=len*width*height;
+ printf("the volume of cuboid is %d",volume);
+ total surface=2*len+2*width+2*height;
+ printf("the total surface area of cuboid is %d",total surface);
+ return 0;
+ }
