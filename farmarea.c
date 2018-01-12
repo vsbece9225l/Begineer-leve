07e58b4762f6539428a45e993aad5c25eba1753c
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-11 21:03:44 -0800

    Create farmarea.c

---
+++ farmarea.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ float a,b,c;
+printf("enter the length:");
+scanf("%f",&a);
+printf("\nenter the breadth:");
+scanf("%f",&b);
+c=a*b;
+printf("the area of farm is %f",c);
+return 0;
+}
