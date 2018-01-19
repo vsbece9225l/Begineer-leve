dbfbe9d68b469b307990f028c948421084414097
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-19 14:00:21 +0530

    Create round.c

---
+++ round.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ float a;
+ printf("enter the floating point number:");
+ scanf("%f",&a);
+ printf("the round of floating point %f is %f",a,round(a));
+ return 0;
+ }
