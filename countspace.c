71b68bdbaf812c471f7680a4e4e5c5becd233408
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-21 11:08:18 +0530

    Create countspace.c

---
+++ countspace.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  char str[50]="hello world";
+  int i,count=0;
+  for(i=0;str[i]!='\0';i++)
+  {
+     if(str[i]=='')
+     {
+       count++;
+       }
+       }
+       printf("the count of space is %d",count);
+       getch();
+       return 0;
+       }
