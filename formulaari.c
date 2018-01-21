401ea2c6c9ff39063c805ada0e29019164c4b253
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-21 09:44:55 +0530

    Create formulaari.c

---
+++ formulaari.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,d,n,s;
+printf("enter a number:");
+scanf("%d",&a);
+printf("enter anumber:");
+scanf("%d",&b);
+printf("enter a number:");
+scanf("%d",&c);
+s=(n/2)*(2*a+(n-1)*d);
+printf("%d",s);
+getch();
+return 0;
+}
+
