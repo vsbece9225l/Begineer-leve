a836a890212a25fa9d797112927d9ea30291d3e7
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-24 07:57:30 -0800

    Create kthsmallest.c

---
+++ kthsmallest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int num[10],n,k,i;
+ printf("enter the number:");
+ scanf("%d%d",&n,&k);
+ for(i=0;i<n;i++)
+ {
+   scanf("%d",&numm[i]);
+   }
+   printf("kth samllest number is %d",num[k-1]);
+   getch();
+   return 0;
+   }
