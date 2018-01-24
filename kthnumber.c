15316c0f78c543028638719d100649d0d819f7e9
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-24 06:55:57 -0800

    Create kthnumber.c

---
+++ kthnumber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int a[10],n,k,i;
+ printf("enter k and n value:");
+ scanf("%d%d",&n,&k);
+ for(i=0;i<n;i++)
+ {
+   scanf("%d",a[k-1]);
+   }
+   return 0;
+   }
