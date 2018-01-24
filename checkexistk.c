9991ae58be6b6f9d90b3685b701f4307192cced5
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-24 07:48:09 -0800

    Create checkexistk.c

---
+++ checkexistk.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int n,k;
+  int num[20];
+  printf("enter the number:");
+  scanf("%d%d",&n,&k);
+  for(i=0;i<n;i++)
+  {
+    scanf("%",num[i]);
+    }
+    printf("%d",num[k-1]);
+    if(k==num[k-1])
+    {
+      printf("yes");
+      }
+      getch();
+      return 0;
+      }
