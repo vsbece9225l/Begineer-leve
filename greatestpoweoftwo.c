e3dfb42608edc5b6cd643dc6db38c49e9703e9ac
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-01-25 16:23:31 +0530

    Create greatestpoweoftwo.c

---
+++ greatestpoweoftwo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int n,n1;
+ printf("enter the number:");
+ scanf("%d",&n);
+ printf("nearest greatest number is");
+ n=n-1;
+ n1=pow(2,n);
+ printf("the power of number is %d",n1);
+ return 0;
+ }
